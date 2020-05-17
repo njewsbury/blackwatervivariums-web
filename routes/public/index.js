7bd786d9d0aba32433d4d3bf84a743f91d8c7782
Nathan Jewsbury <nathan@jewsbury.ca>
2020-05-17 15:19:45 -0500

    Adding public resource router

---
+++ routes/public/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const path = require('path');
+
+const express = require('express');
+const router = new express.Router();
+
+router.get('*.html', (req, res) => {
+    return res.render(path.join(__dirname, 'view', req.path));
+});
+
+router.get('/resources', express.static(path.join(__dirname, 'resources')));
+
+module.exports = router;
